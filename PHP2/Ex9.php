00dd550cdb33e98e4d2531f290cd6be807ba2345
Mamath68 <mathieu.stamm@gmail.Com>
2023-02-21 14:05:17 +0100

    modif exo9, indentation

--- PHP2/Ex9.php
+++ PHP2/Ex9.php
@@ -1,25 +1,24 @@
 <title>Exo 9 : Algo/php</title>";
 
-    <h2>Exercice 9:</h2>
-    <p>
-        Créer une fonction personnalisée permettant d'afficher des boutons radio avec un tableau de
-        <br>
-        valeurs en paramètre ("Monsieur", "Madame", "Mademoiselle").
-        <br><br>
-        <b>Exemple:</b>
-        <br>
-        <code>afficherRadio($nomsRadio);</code>
-    </p>
-    <h3>Réponse:</h3>
-    <?php
-    $nomsRadio = array("Monsieur", "Madame", "Mademoiselle");
-    afficherRadio($nomsRadio);
-    function afficherRadio($nomsRadio) {
-        echo "<form>";
-        foreach($nomsRadio as $nomRadio) {
-            echo "<input type='radio' name='radio'>$nomRadio<br>";
-        }
-        echo "</form>";
+<h2>Exercice 9:</h2>
+<p>
+    Créer une fonction personnalisée permettant d'afficher des boutons radio avec un tableau de valeurs en paramètre
+    ("Monsieur", "Madame", "Mademoiselle").
+    <br><br>
+    <b>Exemple:</b>
+    <br>
+    <code>afficherRadio($nomsRadio);</code>
+</p>
+
+<?php
+$nomsRadio = array("Monsieur", "Madame", "Mademoiselle");
+afficherRadio($nomsRadio);
+function afficherRadio($nomsRadio)
+{
+    echo "<form>";
+    foreach ($nomsRadio as $nomRadio) {
+        echo "<input type='radio' name='radio'>$nomRadio<br>";
     }
-    ?>
-    
+    echo "</form>";
+}
+?>
