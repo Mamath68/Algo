521db44941074b82adf0ee31054397ff7f097188
Mamath68 <mathieu.stamm@gmail.Com>
2023-02-21 15:17:35 +0100

    Exercice Completé

--- PHP2/Ex10.php
+++ PHP2/Ex10.php
@@ -11,15 +11,83 @@
     (<code>submit</code>).
 </p>
 
+
 <?php
-$nomsRadio = array("Monsieur", "Madame", "Mademoiselle");
-afficherRadio($nomsRadio);
-function afficherRadio($nomsRadio)
+
+$nomsInput = array("Nom", "Prénom", "Adresse E-mail", "Ville");
+echo afficherInput($nomsInput);
+function afficherInput($nomsInput)
 {
-    echo "<form>";
-    foreach ($nomsRadio as $nomRadio) {
-        echo "<input type='radio' name='radio'>$nomRadio<br>";
+    echo "<form class='form2'>";
+    foreach ($nomsInput as $nom) {
+        echo "<label for='$nom'>$nom</label><br><input type='text' id='$nom' name='$nom' value=''><br>";
     }
     echo "</form>";
 }
-?>
+
+$elements = array("Quel est votre genre ?", "Homme", "Femme", "Autre / Ne souhaite pas répondre");
+
+alimenterListeDeroulante($elements);
+function alimenterListeDeroulante($elements)
+{
+    echo "<form><select>";
+    foreach ($elements as $el) {
+        echo "<option value='$el'>$el</option>";
+    }
+    echo "<select></form>";
+}
+
+$nomsFormation = array("Développeur Logiciel", "Designer web", "Intégrateur", "Chef de projet");
+afficherRadio($nomsFormation);
+function afficherRadio($nomsFormation)
+{
+    echo "<form>";
+    foreach ($nomsFormation as $nomFormation) {
+        echo "<input type='radio' name='radio'>$nomFormation<br>";
+    }
+    echo "</form>";
+}
+
+echo "<input type='submit' name='submit' id='submit'>"
+
+    ?>
+
+<style>
+    body {
+        margin-left: 100px;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    form {
+        background-color: cadetblue;
+        width: 210px;
+        padding: 1.2em;
+        margin: 0;
+    }
+
+    .form2 {
+        background-color: cadetblue;
+        width: 210px;
+        text-align: center;
+        padding: 1.2em;
+    }
+
+    input[type=submit] {
+        height: 70px;
+        width: 249px;
+        padding: 0;
+        border: solid cadetblue;
+        background-color: cadetblue;
+        font-size: 1.5em;
+        color: white;
+        transition: 0.3s;
+    }
+
+    input[type=submit]:hover {
+        color: white;
+        background-color: blue;
+    }
+</style>
