41d31b187c1697bd7fb6db66673579a634737a27
Mamath68 <mathieu.stamm@gmail.Com>
2023-02-22 09:53:45 +0100

    EX12 php2. dern.modif

--- PHP2/Ex12.php
+++ PHP2/Ex12.php
@@ -1,40 +1,29 @@
-<html>
-    <body>
-        <h1>Algorithmes PHP n°2</h1>
+<title>Exo 10 : Algo/php</title>
+<h1><u>Exercice 12:</u></h1>
+<p>
+    La fonction <code>var_dump ($variable)</code> permet d'afficher les informations d'une variable.
     <br><br>
-    <h2><u>Exercice 12:</u></h2>
-    <p>
-        La fonction <code>var_dump ($variable)</code> permet d'afficher les informations d'une variable.
-        <br><br>
-        Soit le tableau suivant:
-        <br>
-        <code>$tableauValeurs=array(true,"texte",10,25.369,array("valeur1","valeur2"));</code>
-        <br>
-        A l'aide d'une boucle, afficher les informations des variables contenues dans le tableau.
-        <br>
-        
-    </p>
-    <h3>Réponse:</h3>
-    <?php
-    $tableauValeurs=array(true,"texte",10,25.369,array("valeur1","valeur2"));
-    foreach($tableauValeurs as $affiche) {
-        echo var_dump($affiche)."<br>";
+    Soit le tableau suivant:
+    <br>
+    <code>$tableauValeurs=array(true,"texte",10,25.369,array("valeur1","valeur2"));</code>
+    <br>
+    A l'aide d'une boucle, afficher les informations des variables contenues dans le tableau.
+    <br>
+
+</p>
+
+<?php
+$tableauValeurs = array(true, "texte", 10, 25.369, array("valeur1", "valeur2"));
+foreach ($tableauValeurs as $affiche) {
+    echo var_dump($affiche) . "<br>";
+}
+?>
+<style>
+    body {
+        margin-left: 100px;
     }
-    ?>
-    <style>
-        body {
-            margin-left: 100px;
-        }
-        h1 {
-            text-align: center;
-        }
-        span {
-            color:olive;
-        }
-        form {
-            background-color:cadetblue;
-            width: 210px;
-        }
-    </style>
-    </body>
-</html>
+
+    h1 {
+        text-align: center;
+    }
+</style>
